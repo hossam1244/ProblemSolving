6dfbce70406a081b78c8c7494c08b9decb41bf6d
Hossam Rakha <hossam@cocooncreations.net>
2021-02-28 21:03:45 +0200

    removeDublicates

--- src/main/kotlin/array/RemoveDuplicates.kt
+++ src/main/kotlin/array/RemoveDuplicates.kt
@@ -1,7 +1,20 @@
 package array
 
-class RemoveDuplicates {
-    fun removeDuplicates(nums: IntArray): Int {
 
-    }
+fun main() {
+    var nums : IntArray = intArrayOf(0,0,1,1,1,2,2,3,3,4)
+    System.out.print(removeDuplicates(nums))
 }
+
+
+    fun removeDuplicates(nums: IntArray) : Int {
+        if (nums.isEmpty()) return 0
+        var i = 0
+        for (j in 1 until nums.size) {
+            if (nums[j] != nums[i]) {
+                i++
+                nums[i] = nums[j]
+            }
+        }
+        return i + 1
+    }
