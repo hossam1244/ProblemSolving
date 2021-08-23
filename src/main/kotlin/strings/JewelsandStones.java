94abf6a50a79a7d9f3332c53ce3eaddb6d272c4f
Hossam Rakha <hossamrakha0@gmail.com>
2021-08-23 19:29:44 +0000

    solved Jewels And Stones / String problem

--- src/main/kotlin/strings/JewelsandStones.java
+++ src/main/kotlin/strings/JewelsandStones.java
@@ -1,4 +1,28 @@
 package strings;
 
 public class JewelsandStones {
+
+
+    public static void main(String[] args) {
+        String jewels = "aA";
+        String stones = "aAAbbbb";
+        numJewelsInStones(jewels,stones);
+
+    }
+
+
+    static public int numJewelsInStones(String jewels, String stones) {
+        char[] newStones = stones.toCharArray();
+        int count = 0;
+        for (int i=0; i<newStones.length; i++) {
+            if (jewels.contains(newStones[i] + "")) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        return count;
+    }
+
+
+
 }
