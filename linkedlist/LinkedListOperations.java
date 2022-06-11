a5832982c6aa7648aefbc824ace6ebe066d64e43
Hossam Rakha <hossamrakha0@gmail.com>
2022-06-11 16:51:50 +0200

    get middleNode of linkedlist

--- linkedlist/LinkedListOperations.java
+++ linkedlist/LinkedListOperations.java
@@ -5,21 +5,29 @@ import java.util.HashSet;
 
 public class LinkedListOperations {
     public static void main(String[] args) {
+
+
         ListNode head = new ListNode();
         ListNode node1 = new ListNode();
         ListNode node2 = new ListNode();
+        ListNode node3 = new ListNode();
 
         head.val = 1;
         head.next = node1;
       
-        node1.val = 0;
+        node1.val = 2;
         node1.next = node2;
 
-        node2.val = 1;
+        node2.val = 3;
         node2.next = null;
 
+//        node3.val = 4;
+//        node3.next = null;
+
        // deleteDuplicates(head);
-        getDecimalValue(head);
+      //  getDecimalValue(head);
+        middleNode(head);
+
     }
     static public ListNode deleteDuplicates(ListNode head) {
         if(head==null || head.next==null) return head;
@@ -48,6 +56,37 @@ public class LinkedListOperations {
 
 
         return result;
+    }
+
+
+    static public ListNode middleNode(ListNode head) {
+        int length = 0;
+        int middleNode;
+
+        ListNode current  = head;
+        while(current != null) {
+            length++;
+            current = current.next;
+        }
+
+        middleNode = length / 2;
+
+
+        System.out.println(middleNode);
+
+        ListNode newCurrent = head;
+        int steps = 0;
+        while(newCurrent != null) {
+            if (steps == middleNode) {
+                return newCurrent;
+            }
+            steps++;
+            newCurrent = newCurrent.next;
+        }
+
+
+
+        return head;
     }
 
   
