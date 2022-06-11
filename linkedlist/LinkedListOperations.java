6b203957648b028d21d2c89a4a02cc2f84bdfc70
Hossam Rakha <hossamrakha0@gmail.com>
2022-06-11 17:12:39 +0200

    delete node from linkedlist

--- linkedlist/LinkedListOperations.java
+++ linkedlist/LinkedListOperations.java
@@ -2,45 +2,54 @@
 package linkedlist;
 
 import java.util.HashSet;
+import java.util.LinkedList;
 
 public class LinkedListOperations {
+    static ListNode head = new ListNode();
+
     public static void main(String[] args) {
 
 
-        ListNode head = new ListNode();
         ListNode node1 = new ListNode();
         ListNode node2 = new ListNode();
         ListNode node3 = new ListNode();
 
         head.val = 1;
         head.next = node1;
-      
+
         node1.val = 2;
         node1.next = node2;
 
         node2.val = 3;
-        node2.next = null;
+        node2.next = node3;
 
+        node3.val = 4;
+        node3.next = null;
+
+
+        //    node2 node3
 //        node3.val = 4;
 //        node3.next = null;
 
-       // deleteDuplicates(head);
-      //  getDecimalValue(head);
-        middleNode(head);
+        // deleteDuplicates(head);
+        //  getDecimalValue(head);
+        //middleNode(head);
+        deleteNode(node1);
 
     }
+
     static public ListNode deleteDuplicates(ListNode head) {
-        if(head==null || head.next==null) return head;
+        if (head == null || head.next == null) return head;
 
-       ListNode temp = head;
-       while(temp.next != null) {
-        if (temp.val == temp.next.val) temp.next = temp.next.next;
-        else temp = temp.next;   
-      }
-      return head;
+        ListNode temp = head;
+        while (temp.next != null) {
+            if (temp.val == temp.next.val) temp.next = temp.next.next;
+            else temp = temp.next;
+        }
+        return head;
     }
 
-    static public int getDecimalValue(ListNode head)  {
+    static public int getDecimalValue(ListNode head) {
 
 
         int result = 0;
@@ -63,8 +72,8 @@ public class LinkedListOperations {
         int length = 0;
         int middleNode;
 
-        ListNode current  = head;
-        while(current != null) {
+        ListNode current = head;
+        while (current != null) {
             length++;
             current = current.next;
         }
@@ -76,7 +85,7 @@ public class LinkedListOperations {
 
         ListNode newCurrent = head;
         int steps = 0;
-        while(newCurrent != null) {
+        while (newCurrent != null) {
             if (steps == middleNode) {
                 return newCurrent;
             }
@@ -85,18 +94,46 @@ public class LinkedListOperations {
         }
 
 
-
         return head;
     }
 
-  
+
+    static public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        // here we are assiging node1 to node 3
+        // and we are assiging node1 value to node2 value
+        node.next = node.next.next;
+
+        ListNode currentHead = head;
+        while (currentHead != null) {
+            System.out.println(currentHead.val);
+            currentHead = currentHead.next;
+        }
+
+    }
+
+
+    public ListNode reverseList(ListNode head) {
+
+    }
+
+
 }
 
 
 class ListNode {
     int val;
     ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
