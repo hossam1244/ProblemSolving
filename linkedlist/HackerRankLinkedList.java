127cb8f04f16c7ce8c2c2ab67d5c0081bb430084
Hossam Rakha <hossamrakha0@gmail.com>
2022-06-18 16:20:50 +0200

    insertNodeAtPosition

--- linkedlist/HackerRankLinkedList.java
+++ linkedlist/HackerRankLinkedList.java
@@ -11,19 +11,19 @@ public class HackerRankLinkedList {
         ListNode node2 = new ListNode();
         ListNode node3 = new ListNode();
 
-//        head.val = 1;
-//        head.next = node1;
-//
-//        node1.val = 2;
-//        node1.next = node2;
-//
-//        node2.val = 3;
-//        node2.next = node3;
-//
-//        node3.val = 4;
-//        node3.next = null;
+        head.val = 1;
+        head.next = node1;
 
-        ListNode newHead = insertNodeAtHead(head, 5);
+        node1.val = 2;
+        node1.next = node2;
+
+        node2.val = 3;
+        node2.next = node3;
+
+        node3.val = 5;
+        node3.next = null;
+
+        ListNode newHead = insertNodeAtPosition(head, 4,3);
         while (newHead != null) {
             System.out.println(newHead.val);
             newHead = newHead.next;
@@ -77,6 +77,29 @@ public class HackerRankLinkedList {
         ListNode current = newNode;
 
         return current;
+
+
+    }
+
+    static ListNode insertNodeAtPosition(ListNode head, int data, int position) {
+        ListNode newNode = new ListNode();
+        newNode.val = data;
+        int currentPos = 0;
+
+        // 1 2 3 5 null
+        ListNode current = head;
+        while (current != null) {
+            currentPos++;
+            if (currentPos == position) {
+                newNode.next = current.next;
+                current.next = newNode;
+                return head;
+            }
+            current = current.next;
+        }
+
+        return head;
+
 
 
     }
